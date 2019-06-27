aa67ec0982e828f110d84d605d014a3176bfce52
Daniel Kats <daniel_kats@symantec.com>
2019-06-27 02:13:04 -0400

    extract answers and top-level comments from pokemondb

--- parse_pokemondb_post.py
+++ parse_pokemondb_post.py
@@ -9,7 +9,7 @@ import os
 import json
 
 
-def parse_comment_soup(comment: BeautifulSoup):
+def parse_answer_soup(comment: BeautifulSoup):
     # get metadata
     # mainly we care about the votes
     net_vote_count = int(comment.find('span', {'class': 'qa-netvote-count-data'}).text)
@@ -51,6 +51,20 @@ def parse_question_soup(soup: BeautifulSoup):
     }
 
 
+def parse_question_comment_soup(soup: BeautifulSoup):
+    # net_vote_count = int(soup.find('span', {'class': 'qa-netvote-count-data'}).text)
+    content_soup = soup.find(
+        'div',
+        {'class': 'qa-c-item-content'}
+    ).find(
+        'div',
+        {'itemprop': 'text'}
+    )
+    return {
+        # 'net_vote_count': net_vote_count,
+        'content_text': html2text(content_soup.prettify()),
+        'content_original_markup': content_soup.prettify(),
+    }
 
 
 def extract_content(soup: BeautifulSoup):
@@ -63,7 +77,8 @@ def extract_content(soup: BeautifulSoup):
     main.find('div', {'class': 'qa-page-links'}).decompose()
 
     results = {
-        'comments': []
+        'question_comments': [],
+        'answers': []
     }
 
     # this is the question
@@ -72,11 +87,14 @@ def extract_content(soup: BeautifulSoup):
         {'class': 'qa-q-view'}
     )
     results['question'] = parse_question_soup(q_soup)
-    # pprint(q_data['content_text'])
 
-    # these are the comments
-    for comment in main.findAll('div', {'class': 'qa-a-list-item'}):
-        results['comments'].append(parse_comment_soup(comment))
+    # these are the comments on the question only
+    for comment in main.find('div', {'class': 'qa-q-view-c-list'}).findAll('div', {'class': 'qa-c-list-item'}):
+        results['question_comments'].append(parse_question_comment_soup(comment))
+
+    # these are the answers
+    for answer in main.findAll('div', {'class': 'qa-a-list-item'}):
+        results['answers'].append(parse_answer_soup(answer))
     return results
 
 
@@ -87,19 +105,20 @@ def parse(fname: str):
     return extract_content(soup)
 
 
-def save(out_dir: str, results: dict):
-    fname = os.path.join(out_dir, 'parsed.json')
-    with open(fname, 'w') as f:
+def save(out_fname: str, results: dict):
+    with open(out_fname, 'w') as f:
         json.dump(results, f, sort_keys=True, indent=4)
-    return fname
 
 
 if __name__ == '__main__':
-    # fname = './data/pokemondb-post/raw/What is a good in-game team for Omega Ruby and Alpha Sapphire  - PokéBase Pokémon Answers.html'
     dir = './data/pokemondb-post/raw'
-    out_dir = './data/pokemondb-post/parsed'
+    q_id = '222207'
+    out_fname = './data/pokemondb-post/parsed/pokemondb-{}.json'.format(q_id)
     page = 1
-    results = {'comments': []}
+    results = {
+        'question_comments': [],
+        'answers': [],
+    }
     for fname in sorted(os.listdir(dir)):
         if fname.endswith('.html'):
             path = os.path.join(dir, fname)
@@ -107,7 +126,8 @@ if __name__ == '__main__':
             page_r = parse(path)
             if page == 1:
                 results['question'] = page_r['question']
-            results['comments'].extend(page_r['comments'])
+            results['question_comments'].extend(page_r['question_comments'])
+            results['answers'].extend(page_r['answers'])
             page += 1
-    out_fname = save(out_dir, results)
+    save(out_fname, results)
     print('Wrote to %s' % out_fname)
